a9f5c12ce05b1094a75bb40990acff45af5701ce
kryparnold <emirhanpgt@gmail.com>
2024-04-24 10:05:19 +0300

    added appear and shake animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,15 +3,28 @@ import type { Config } from "tailwindcss";
 const config: Config = {
 	content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
 	theme: {
-		keyframes: {
-			blink: {
-				"100%": { opacity: "0" },
+		extend: {
+			keyframes: {
+				blink: {
+					"100%": { opacity: "0" },
+				},
+				appear: {
+					"100%": { opacity: "100" },
+				},
+				shake: {
+					"0%, 100%": { transform: "rotate(0)" },
+					"10%, 30%, 50%, 70%, 90%": { transform: "rotate(-10deg)" },
+					"20%, 40%, 60%, 80%": { transform: "rotate(10deg)" },
+				},
 			},
-		},
-		animation: {
-			blink: "blink ease-out 0.8s infinite",
+			animation: {
+				blink: "blink ease-out 0.8s infinite",
+				appear: "appear 1s forwards",
+				shake: "shake 0.7s ease-in-out forwards",
+			},
 		},
 	},
 	plugins: [],
 };
+
 export default config;
