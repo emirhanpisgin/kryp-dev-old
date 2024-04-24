82ab3a899cc05f13984860a8370f0d97febc8e2b
kryparnold <emirhanpgt@gmail.com>
2024-04-24 08:38:36 +0300

    added blink animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,16 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
 	content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
-	theme: {},
+	theme: {
+		keyframes: {
+			blink: {
+				"100%": { opacity: "0" },
+			},
+		},
+		animation: {
+			blink: "blink ease-out 0.8s infinite",
+		},
+	},
 	plugins: [],
 };
 export default config;
