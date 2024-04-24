2deb4d96313e968e684d556d054d1af701fd31e9
kryparnold <emirhanpgt@gmail.com>
2024-04-24 08:39:14 +0300

    implemented landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import TypeWriter from "@/components/TypeWriter";
+
 export default function Home() {
     return (
-        <main>
-            Hello World!
+        <main className="grid place-items-center h-full bg-inherit">
+            <div className="flex flex-col gap-5">
+                <TypeWriter text="Kryp.Dev" duration={1} className="text-7xl" />
+            </div>
         </main>
     );
 }
