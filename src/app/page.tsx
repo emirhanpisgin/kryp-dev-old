6307a2fcdb70e2b65aef9498375c002338b8f674
kryparnold <emirhanpgt@gmail.com>
2024-04-25 09:10:47 +0300

    styling changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,20 +35,25 @@ export default function Home() {
 
         linksRef.current.style.maxHeight = "100vh";
 
-        setTimeout(() => {
-            if (!linksRef.current) return;
-            linksRef.current.classList.add("animate-appear");
-        }, 1200)
+        const linkElements = linksRef.current.childNodes as NodeListOf<HTMLDivElement>;
 
+        setTimeout(() => {
+            for (let i = 0; i < linkElements.length; i++) {
+                const linkElement = linkElements.item(i);
+
+                linkElement.style.opacity = "100";
+                linkElement.style.transform = "translateY(0)";
+            }
+        }, 500);
     }
 
     return (
         <main className="grid place-items-center h-full bg-inherit">
             <div className="flex flex-col gap-5 items-center">
                 <TypeWriter text="Kryp.Dev" duration={1} onWritingEnd={onWritingEnd} className="text-6xl md:text-8xl select-none" />
-                <div ref={linksRef} className="flex gap-8 justify-evenly max-h-0 h-auto opacity-0 duration-[3s] transition-max-height">
+                <div ref={linksRef} className="flex gap-8 justify-evenly max-h-0 h-auto duration-[3s] transition-max-height">
                     {links.map((link, index) => (
-                        <div key={index} className="border-2 p-3 rounded-xl cursor-pointer hover:animate-shake">
+                        <div key={index} style={{ transitionDelay: `${(index + 1) * 200}ms` }} className="border-2 p-3 -translate-y-1/3 rounded-xl cursor-pointer duration-500 hover:animate-shake opacity-0 transition-all">
                             <a href={link.url} target="_blank">
                                 <link.iconComponent className="size-8 md:size-12" />
                             </a>
