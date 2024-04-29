d86fd5089fe6ff06cec9d3218f76f18d9db5ff8b
kryparnold <emirhanpgt@gmail.com>
2024-04-29 22:33:11 +0300

    styling logic changes

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,9 +1,7 @@
-import { ClockIcon, PencilIcon } from "@/components/Icons";
+import BlogCard from "@/components/BlogCard";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
-import { timePassed } from "@/utils";
 import { allDocs } from "contentlayer/generated";
 import { Metadata } from "next";
-import Link from "next/link";
 
 export const metadata: Metadata = {
     title: 'Kryp.Dev Blogs',
@@ -22,26 +20,7 @@ export default async function Blogs() {
                 </div>
                 <div className="mt-5 flex flex-col md:gap-6">
                     {allDocs.map((doc, index) => (
-                        <Link href={doc.slug} key={index} className="border-b-2 p-3 md:rounded-xl md:border-2 flex flex-col first:border-t-2">
-                            <div className="text-2xl my-1">
-                                {doc.title}
-                            </div>
-                            <div className="whitespace-pre-wrap text-xl my-1">
-                                {doc.description}
-                            </div>
-                            <div className="flex justify-between">
-                                <div className="flex items-center">
-                                    <PencilIcon className="size-5 mr-1" />
-                                    <div>
-                                        {doc.author}
-                                    </div>
-                                </div>
-                                <div className="flex items-center">
-                                    <ClockIcon className="size-5 mr-1"/>
-                                    {timePassed(new Date(doc.createdAt))}
-                                </div>
-                            </div>
-                        </Link>
+                        <BlogCard doc={doc} key={index} />
                     ))}
                 </div>  
             </div>
---
+++ src/components/BlogCard.tsx
@@ -0,0 +1,41 @@
+"use client";
+import { timePassed } from "@/utils";
+import { Doc } from "contentlayer/generated";
+import Link from "next/link";
+import { PencilIcon, ClockIcon } from "./Icons";
+import { useEffect, useState } from "react";
+
+interface BlogCardProps {
+    doc: Doc;
+}
+
+export default function BlogCard({ doc }: BlogCardProps) {
+    const [timeStamp, setTimeStamp] = useState("");
+
+    useEffect(() => {
+        setTimeStamp(timePassed(new Date(doc.createdAt)));
+    }, [doc.createdAt])
+
+    return (
+        <Link href={doc.slug} className="border-b-2 p-3 md:rounded-xl md:border-2 flex flex-col first:border-t-2">
+            <div className="text-2xl my-1">
+                {doc.title}
+            </div>
+            <div className="whitespace-pre-wrap text-xl my-1">
+                {doc.description}
+            </div>
+            <div className="flex justify-between">
+                <div className="flex items-center">
+                    <PencilIcon className="size-5 mr-1" />
+                    <div>
+                        {doc.author}
+                    </div>
+                </div>
+                <div className="flex items-center">
+                    <ClockIcon className="size-5 mr-1" />
+                    {timePassed(new Date(doc.createdAt))}
+                </div>
+            </div>
+        </Link>
+    );
+}
