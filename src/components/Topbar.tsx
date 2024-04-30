449bdc631b579b0cc03a9dec4c68559621582a97
kryparnold <emirhanpgt@gmail.com>
2024-04-30 21:21:09 +0300

    styling changes

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -3,8 +3,8 @@ import { URLArrowIcon } from "./Icons";
 
 export default function Topbar() {
     return (
-        <div className="fixed top-0 w-full h-8">
-            <Link href={"https://github.com/kryparnold/kryp-dev"} target="_blank" className="flex items-center gap-1 justify-end p-5 text-2xl text-white/30 hover:text-white transition-colors">
+        <div className="absolute top-0 w-full flex justify-end items-center">
+            <Link href={"https://github.com/kryparnold/kryp-dev"} target="_blank" className="flex items-center gap-1 p-5 text-2xl text-white/30 hover:text-white transition-colors">
                 <URLArrowIcon className="size-8 -rotate-[135deg] text-inherit" />
                 Source
             </Link>
