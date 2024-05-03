05a6d001f74002684dca169db8ef8e013d55dc0c
kryparnold <emirhanpgt@gmail.com>
2024-05-03 23:56:05 +0300

    hotter fix

--- src/components/ChangeTheme.tsx
+++ src/components/ChangeTheme.tsx
@@ -6,10 +6,12 @@ import { Moon, Sun } from "./Icons";
 export default function ChangeTheme() {
     const { setTheme, resolvedTheme } = useTheme();
 
+    const defaultTheme = resolvedTheme === "system" ? "dark" : resolvedTheme;
+
     return (
         <div className="relative size-8 cursor-pointer">
-            <Sun className={`size-8 text-black transition-opacity absolute top-0 left-0 ${resolvedTheme === "light" ? "opacity-100" : "opacity-0 pointer-events-none"}`} onClick={() => setTheme("dark")} />
-            <Moon className={`size-8 transition-opacity absolute top-0 left-0 ${resolvedTheme === "dark" ? "opacity-100" : "opacity-0 pointer-events-none"}`} onClick={() => setTheme("light")} />
+            <Sun className={`size-8 text-black transition-opacity absolute top-0 left-0 ${defaultTheme === "light" ? "opacity-100" : "opacity-0 pointer-events-none"}`} onClick={() => setTheme("dark")} />
+            <Moon className={`size-8 transition-opacity absolute top-0 left-0 ${defaultTheme === "dark" ? "opacity-100" : "opacity-0 pointer-events-none"}`} onClick={() => setTheme("light")} />
         </div>
     );
 }
