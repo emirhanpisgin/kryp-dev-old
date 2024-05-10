c2e16288e8bb16f513c7ac07773674b55e317d4d
kryparnold <emirhanpgt@gmail.com>
2024-05-10 04:30:19 +0300

    static blogs page

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -9,8 +9,6 @@ export const metadata: Metadata = {
     description: 'My Blogs',
 }
 
-export const dynamic = "force-dynamic";
-
 export default async function Blogs() {
     const blogs = await prisma.blog.findMany();
 
