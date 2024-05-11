97ea756a8839a7206b761ece103254b10bb3bcc6
kryparnold <emirhanpgt@gmail.com>
2024-05-11 15:03:53 +0300

    dropped react-markdown and setup next-mdx-remote

--- src/components/Mdx.tsx
+++ src/components/Mdx.tsx
@@ -1,10 +1,7 @@
+"use client";
 import { cn } from "@/utils";
 import React from "react";
-import remarkGfm from "remark-gfm";
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { oneDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
-import ReactMarkdown from 'react-markdown'
-import rehypeSlug from "rehype-slug";
+import { MDXRemote } from "next-mdx-remote";
 
 interface MdxProps {
     content: string;
@@ -20,22 +17,8 @@ const components = {
     p: ({ className, ...props }: React.ComponentProps<'h1'>) => (
         <p className={cn("text-xl my-3 w-full", className)} {...props} />
     ),
-    code: ({ className, children, ...props }: React.ComponentProps<'code'>) => {
-        const match = /language-(\w+)/.exec(className || '');
-
-        return match ? (
-            //@ts-expect-error
-            <SyntaxHighlighter codeTagProps={{ className: cn("!whitespace-pre-wrap", className) }} customStyle={{ borderRadius: "0.5rem", margin: "0" }} style={oneDark} language={match[1]} {...props}>
-                {String(children).replaceAll("\\\"", "\"").replaceAll("\\t", "    ")}
-            </SyntaxHighlighter>
-        ) : (
-            <code className={className} {...props}>
-                {children}
-            </code>
-        );
-    },
     pre: ({ className, ...props }: React.ComponentProps<'pre'>) => (
-        <pre className={cn("rounded-lg my-2", className)} {...props} />
+        <pre className={cn("rounded-lg my-2 p-4", className)} {...props} />
     ),
     li: ({ className, ...props }: React.ComponentProps<'li'>) => (
         <li className={cn("text-xl", className)} {...props} />
@@ -46,10 +29,6 @@ const components = {
 }
 
 export default function Mdx({ content }: MdxProps) {
-
-    return (
-        <ReactMarkdown components={components} remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeSlug]}>
-            {content.replace(/\\n/g, '\n')}
-        </ReactMarkdown>
-    );
+    //@ts-expect-error
+    return <MDXRemote compiledSource={content} components={components} />;
 }
