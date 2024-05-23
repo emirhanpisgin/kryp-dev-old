0a3acfa9ca6110e29fe45b030a39d393dfa69e4e
kryparnold <emirhanpgt@gmail.com>
2024-05-23 13:21:18 +0300

    rehype-pretty-code downgrade

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: false,
-	experimental: {
-		serverMinification: false,
-	},
+	reactStrictMode: false
 };
 
 export default nextConfig;
